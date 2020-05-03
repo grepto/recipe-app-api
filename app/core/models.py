800d8c8f984eff657e3b7462cca7f57ce024cddf
grepto <grepto@gmail.com>
2020-05-03 16:28:50 +0300

    Update admin to custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -8,8 +8,7 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
         if not email:
-            raise ValueError('User must have an email address')
-
+            raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -17,12 +16,10 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password):
-        """Creates and saves a new superuser"""
-
+        """Creates and saves a new super user"""
         user = self.create_user(email, password)
-        user.is_stuff = True
+        user.is_staff = True
         user.is_superuser = True
-
         user.save(using=self._db)
 
         return user
@@ -33,7 +30,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_stuff = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -40,4 +40,4 @@ class ModelTests(TestCase):
         )
 
         self.assertTrue(user.is_superuser)
-        self.assertTrue(user.is_stuff)
+        self.assertTrue(user.is_staff)
