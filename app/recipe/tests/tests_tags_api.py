6701541a367a7e6e15a007d40bfbdeceb6dd08cd
grepto <grepto@gmail.com>
2020-05-24 14:50:45 +0300

    added create tag feature

--- app/recipe/tests/tests_tags_api.py
+++ app/recipe/tests/tests_tags_api.py
@@ -61,3 +61,22 @@ class PrivteTegsApiTests(TestCase):
 
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], authorized_user_tag.name)
+
+    def test_create_tag_successfull(self):
+        """Test creating a new tag"""
+        playload = {'name': 'Test tag'}
+        self.client.post(TAGS_URL, playload)
+
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=playload['name']
+        ).exists()
+
+        self.assertTrue(exists)
+
+    def test_create_tag_invalid(self):
+        """Test creating a new tag invalid payload"""
+        playload = {'name': ''}
+        res = self.client.post(TAGS_URL, playload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
